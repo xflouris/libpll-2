454082810d6f73b6724abb9648b15a2b9099e60b
Rudolf Biczok <rcbiczok@gmail.com>
2018-04-11 19:33:07 +0200

    Modify benchmark execution script

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -3,29 +3,92 @@ experimentDir=$PWD/experiment
 
 ARCH=(avx2 "avx2 sr" avx512f "avx512f sml")
 ALPHA_VALUES=("0.1" "0.1,0.75,1.5")
-CATEGORIES=(1 5 25)
-PINVAR=(0.0 0.3 0.5 0.6)
-SITES=(1000 1000 100000 1000000)
+CATEGORIES=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16)
+PINVAR=(0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.9)
+PMATRIX_ITR=(1 100 200 300 400 500 600 700 800 900 1000)
+SITES=(1000 2000 3000 4000 5000 6000 7000 8000 9000 10000 20000 30000 40000 50000 60000 70000 80000 90000 100000)
 
 mkdir -p $experimentDir
 
-otherDir=$experimentDir/other
-mkdir -p ${otherDir}
-
+echo "P-Invar Benchmark"
+pinvarDir=$experimentDir/pinvar
+mkdir -p ${pinvarDir}
 for arch in "${ARCH[@]}"
 do
     for pinvar in "${PINVAR[@]}"
     do
-        #subAlphaValues=${alphaValues//,/__}
-        #fileName=${arch/ /_}__${pinvar/./_}__${subAlphaValues//./_}__${categories}__${sites}
-        fileName=${otherDir}/${arch/ /_}__${pinvar/./_}.txt
-        ./obj/derivatives-aa-benchmark ${arch} -p-invar=${pinvar} > $fileName
+        fileName=${pinvarDir}/${arch/ /_}__${pinvar/./_}.txt
+        ./obj/derivatives-aa-benchmark ${arch} -p-invar=${pinvar} -n-sites=100000 > $fileName
     done
 done
 
-#mkdir -p $MAIN_DIR/detail
-#./obj/derivatives-aa-benchmark-simple avx2        > $MAIN_DIR/detail/avx2.txt
-#./obj/derivatives-aa-benchmark-simple avx2 sr     > $MAIN_DIR/detail/avx2_sr.txt
-#./obj/derivatives-aa-benchmark-simple avx512f     > $MAIN_DIR/detail/avx512.txt
-#./obj/derivatives-aa-benchmark-simple avx512f sml > $MAIN_DIR/detail/avx512_sml.txt
-#action.py $MAIN_DIR/detail/*.txt     > $MAIN_DIR/detail/detail_summary.csv
+echo "Sites Benchmark"
+sitesDir=$experimentDir/sites
+mkdir -p ${sitesDir}
+for arch in "${ARCH[@]}"
+do
+    for sites in "${SITES[@]}"
+    do
+        fileName=${sitesDir}/${arch/ /_}__${sites}.txt
+        ./obj/derivatives-aa-benchmark ${arch} -n-sites=${sites} > $fileName
+    done
+done
+
+echo "Category Benchmark"
+categoriesDir=$experimentDir/categories
+mkdir -p ${categoriesDir}
+for arch in "${ARCH[@]}"
+do
+    for categories in "${CATEGORIES[@]}"
+    do
+        fileName=${categoriesDir}/${arch/ /_}__${categories}.txt
+        ./obj/derivatives-aa-benchmark ${arch} -n-categories=${categories} -n-sites=100000 > $fileName
+    done
+done
+
+echo "Alpha Values Benchmark"
+alphaDir=$experimentDir/alpha
+mkdir -p ${alphaDir}
+for arch in "${ARCH[@]}"
+do
+    for alphaValues in "${ALPHA_VALUES[@]}"
+    do
+        subAlphaValues=${alphaValues//,/__}
+        fileName=${alphaDir}/${arch/ /_}__${subAlphaValues//./_}.txt
+        ./obj/derivatives-aa-benchmark ${arch} -alpha=${alphaValues} -n-sites=100000 > $fileName
+    done
+done
+
+echo "PMatrix Benchmark"
+pmatrixDir=$experimentDir/pmatrix
+mkdir -p ${pmatrixDir}
+for arch in "${ARCH[@]}"
+do
+    for pmatrix in "${PMATRIX_ITR[@]}"
+    do
+        fileName=${pmatrixDir}/${arch/ /_}__${pmatrix}.txt
+        ./obj/derivatives-aa-benchmark ${arch} -n-pmatrix-itr=${pmatrix} -n-sites=100000 > $fileName
+    done
+done
+
+echo "Full Benchmark"
+pinvarDir=$experimentDir/full
+mkdir -p ${pinvarDir}
+for arch in "${ARCH[@]}"
+do
+    for pinvar in "${PINVAR[@]}"
+    do
+        for alphaValues in "${ALPHA_VALUES[@]}"
+        do
+            for categories in "${CATEGORIES[@]}"
+            do
+                for sites in "${SITES[@]}"
+                do
+                    subAlphaValues=${alphaValues//,/__}
+                    fileName=${pinvarDir}/${arch/ /_}__${pinvar/./_}__${subAlphaValues//./_}__${categories}__${sites}.txt
+                    ./obj/derivatives-aa-benchmark ${arch} -alpha=${alphaValues} -p-invar=${pinvar} -n-categories=${categories} -n-sites=${sites} > $fileName
+                done
+            done
+        done
+    done
+done
