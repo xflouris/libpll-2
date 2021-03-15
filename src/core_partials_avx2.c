4e58a722e5890fc35fde7a2a130a22c65413aaf1
Alexey Kozlov <alexey.kozlov@h-its.org>
2021-03-16 00:18:36 +0100

    add specialized update_partials kernel for 16-state phased genotype model

--- src/core_partials_avx2.c
+++ src/core_partials_avx2.c
@@ -817,6 +817,195 @@ PLL_EXPORT void pll_core_update_partial_ii_20x20_avx2(unsigned int sites,
   }
 }
 
+PLL_EXPORT void pll_core_update_partial_ii_16x16_avx2(unsigned int sites,
+                                                unsigned int rate_cats,
+                                                double * parent_clv,
+                                                unsigned int * parent_scaler,
+                                                const double * left_clv,
+                                                const double * right_clv,
+                                                const double * left_matrix,
+                                                const double * right_matrix,
+                                                const unsigned int * left_scaler,
+                                                const unsigned int * right_scaler,
+                                                unsigned int attrib)
+{
+  unsigned int i,k,n;
+
+  const double * lmat;
+  const double * rmat;
+
+  unsigned int states = 16;
+  unsigned int states_padded = states;
+  unsigned int span_padded = states_padded * rate_cats;
+
+  /* scaling-related stuff */
+  unsigned int scale_mode;  /* 0 = none, 1 = per-site, 2 = per-rate */
+  unsigned int scale_mask;
+  unsigned int init_mask;
+  __m256d v_scale_threshold = _mm256_set1_pd(PLL_SCALE_THRESHOLD);
+  __m256d v_scale_factor = _mm256_set1_pd(PLL_SCALE_FACTOR);
+
+  if (!parent_scaler)
+  {
+    /* scaling disabled / not required */
+    scale_mode = init_mask = 0;
+  }
+  else
+  {
+    /* determine the scaling mode and init the vars accordingly */
+    scale_mode = (attrib & PLL_ATTRIB_RATE_SCALERS) ? 2 : 1;
+    init_mask = (scale_mode == 1) ? 0xF : 0;
+    const size_t scaler_size = (scale_mode == 2) ? sites * rate_cats : sites;
+    /* add up the scale vector of the two children if available */
+    fill_parent_scaler(scaler_size, parent_scaler, left_scaler, right_scaler);
+  }
+
+  size_t displacement = (states_padded - states) * (states_padded);
+
+  /* compute CLV */
+  for (n = 0; n < sites; ++n)
+  {
+    lmat = left_matrix;
+    rmat = right_matrix;
+    scale_mask = init_mask;
+
+    for (k = 0; k < rate_cats; ++k)
+    {
+      unsigned int rate_mask = 0xF;
+
+      __m256d v_lclv[5];
+      __m256d v_rclv[5];
+      for (i = 0; i < 5; ++i)
+      {
+        v_lclv[i]    = _mm256_load_pd(left_clv + i*4);
+        v_rclv[i]    = _mm256_load_pd(right_clv + i*4);
+      }
+
+      /* iterate over quadruples of rows */
+      for (i = 0; i < states_padded; i += 4)
+      {
+        __m256d v_mat;
+
+        __m256d v_terma0 = _mm256_setzero_pd();
+        __m256d v_termb0 = _mm256_setzero_pd();
+        __m256d v_terma1 = _mm256_setzero_pd();
+        __m256d v_termb1 = _mm256_setzero_pd();
+        __m256d v_terma2 = _mm256_setzero_pd();
+        __m256d v_termb2 = _mm256_setzero_pd();
+        __m256d v_terma3 = _mm256_setzero_pd();
+        __m256d v_termb3 = _mm256_setzero_pd();
+
+        /* point to the four rows of the left matrix */
+        const double * lm0 = lmat;
+        const double * lm1 = lm0 + states_padded;
+        const double * lm2 = lm1 + states_padded;
+        const double * lm3 = lm2 + states_padded;
+
+        /* point to the four rows of the right matrix */
+        const double * rm0 = rmat;
+        const double * rm1 = rm0 + states_padded;
+        const double * rm2 = rm1 + states_padded;
+        const double * rm3 = rm2 + states_padded;
+
+        /* iterate over quadruples of columns */
+        COMPUTE_II_QCOL(0, 0);
+        COMPUTE_II_QCOL(1, 4);
+        COMPUTE_II_QCOL(2, 8);
+        COMPUTE_II_QCOL(3, 12);
+
+        /* point pmatrix to the next four rows */
+        lmat = lm3 + states_padded;
+        rmat = rm3 + states_padded;
+
+        /* compute terma */
+
+        __m256d xmm0 = _mm256_unpackhi_pd(v_terma0,v_terma1);
+        __m256d xmm1 = _mm256_unpacklo_pd(v_terma0,v_terma1);
+
+        __m256d xmm2 = _mm256_unpackhi_pd(v_terma2,v_terma3);
+        __m256d xmm3 = _mm256_unpacklo_pd(v_terma2,v_terma3);
+
+        xmm0 = _mm256_add_pd(xmm0,xmm1);
+        xmm1 = _mm256_add_pd(xmm2,xmm3);
+
+        xmm2 = _mm256_permute2f128_pd(xmm0,xmm1, _MM_SHUFFLE(0,2,0,1));
+
+        xmm3 = _mm256_blend_pd(xmm0,xmm1,12);
+
+        __m256d v_terma_sum = _mm256_add_pd(xmm2,xmm3);
+
+        /* compute termb */
+
+        xmm0 = _mm256_unpackhi_pd(v_termb0,v_termb1);
+        xmm1 = _mm256_unpacklo_pd(v_termb0,v_termb1);
+
+        xmm2 = _mm256_unpackhi_pd(v_termb2,v_termb3);
+        xmm3 = _mm256_unpacklo_pd(v_termb2,v_termb3);
+
+        xmm0 = _mm256_add_pd(xmm0,xmm1);
+        xmm1 = _mm256_add_pd(xmm2,xmm3);
+
+        xmm2 = _mm256_permute2f128_pd(xmm0,xmm1, _MM_SHUFFLE(0,2,0,1));
+
+        xmm3 = _mm256_blend_pd(xmm0,xmm1,12);
+
+        __m256d v_termb_sum = _mm256_add_pd(xmm2,xmm3);
+
+        __m256d v_prod = _mm256_mul_pd(v_terma_sum,v_termb_sum);
+
+        /* check if scaling is needed for the current rate category */
+        __m256d v_cmp = _mm256_cmp_pd(v_prod, v_scale_threshold, _CMP_LT_OS);
+        rate_mask = rate_mask & _mm256_movemask_pd(v_cmp);
+
+        _mm256_store_pd(parent_clv+i, v_prod);
+      }
+
+      if (scale_mode == 2)
+      {
+        /* PER-RATE SCALING: if *all* entries of the *rate* CLV were below
+         * the threshold then scale (all) entries by PLL_SCALE_FACTOR */
+        if (rate_mask == 0xF)
+        {
+          for (i = 0; i < states_padded; i += 4)
+          {
+            __m256d v_prod = _mm256_load_pd(parent_clv + i);
+            v_prod = _mm256_mul_pd(v_prod, v_scale_factor);
+            _mm256_store_pd(parent_clv + i, v_prod);
+          }
+          parent_scaler[n*rate_cats + k] += 1;
+        }
+      }
+      else
+        scale_mask = scale_mask & rate_mask;
+
+      /* reset pointers to point to the start of the next p-matrix, as the
+         vectorization assumes a square states_padded * states_padded matrix,
+         even though the real matrix is states * states_padded */
+      lmat -= displacement;
+      rmat -= displacement;
+
+      parent_clv += states_padded;
+      left_clv   += states_padded;
+      right_clv  += states_padded;
+    }
+
+    /* if *all* entries of the site CLV were below the threshold then scale
+       (all) entries by PLL_SCALE_FACTOR */
+    if (scale_mask == 0xF)
+    {
+      parent_clv -= span_padded;
+      for (i = 0; i < span_padded; i += 4)
+      {
+        __m256d v_prod = _mm256_load_pd(parent_clv + i);
+        v_prod = _mm256_mul_pd(v_prod,v_scale_factor);
+        _mm256_store_pd(parent_clv + i, v_prod);
+      }
+      parent_clv += span_padded;
+      parent_scaler[n] += 1;
+    }
+  }
+}
+
 PLL_EXPORT void pll_core_update_partial_ii_avx2(unsigned int states,
                                                 unsigned int sites,
                                                 unsigned int rate_cats,
@@ -843,6 +1032,21 @@ PLL_EXPORT void pll_core_update_partial_ii_avx2(unsigned int states,
   {
     /* TODO: Implement avx2 4x4 case */
     pll_core_update_partial_ii_4x4_avx(sites,
+                                       rate_cats,
+                                       parent_clv,
+                                       parent_scaler,
+                                       left_clv,
+                                       right_clv,
+                                       left_matrix,
+                                       right_matrix,
+                                       left_scaler,
+                                       right_scaler,
+                                       attrib);
+    return;
+  }
+  else if (states == 16)
+  {
+    pll_core_update_partial_ii_16x16_avx2(sites,
                                        rate_cats,
                                        parent_clv,
                                        parent_scaler,
